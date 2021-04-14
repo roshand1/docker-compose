28863341a15e3b6e53fe89b273916f1728281df3
Roshan Kandel <rkandel@healthgrades.com>
2021-04-14 09:48:38 -0600

    created new text

--- ReactBolerPlateWithWebpackExpress/src/components/Gallery/Gallery.jsx
+++ ReactBolerPlateWithWebpackExpress/src/components/Gallery/Gallery.jsx
@@ -24,7 +24,7 @@ function Gallery () {
 
     return (
       <div className="App">
-        <h3>Images</h3>
+        <h3>Our Pictures</h3>
         <div className="image-container">
                 <img className="displayImage" src={displayImage} alt="sample" />
         </div>
